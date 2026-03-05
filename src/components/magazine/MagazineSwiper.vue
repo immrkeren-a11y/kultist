7b12f2799f29cebc91d6c721bed0c80170744562
Сергей Процай <spro1103@mail.ru>
2026-03-05 14:14:51 +0300

    swiper imported

--- src/components/magazine/MagazineSwiper.vue
+++ src/components/magazine/MagazineSwiper.vue
@@ -1,10 +1,57 @@
-<script setup lang="ts">
-</script>
-
 <template>
-  <div>
+
+  <div class="magazine">
+
+    <Swiper
+      :slides-per-view="1"
+      :space-between="20"
+      :navigation="true"
+      :pagination="{ clickable: true,
+        type: 'progressbar'
+      }"
+    >
+
+      <SwiperSlide
+        v-for="(page, index) in pages"
+        :key="index"
+      >
+        <img
+          :src="page"
+          class="magazine-page"
+        >
+      </SwiperSlide>
+
+    </Swiper>
+
   </div>
 </template>
 
+<script setup lang="ts">
+
+import { Swiper, SwiperSlide } from 'swiper/vue'
+
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+
+
+defineProps<{
+  pages: string[]
+}>()
+
+</script>
+
 <style>
-</style>
+.magazine {
+
+  width: 900px;
+  margin: 120px auto;
+
+}
+
+.magazine-page {
+
+  width: 100%;
+  height: auto;
+
+}</style>
---
+++ src/views/IssueView.vue
@@ -0,0 +1,27 @@
+<template>
+
+  <div class="issue-page">
+
+    <MagazineSwiper
+      v-if="issue"
+      :pages="issue.pages"
+    />
+
+  </div>
+
+</template>
+
+<script setup lang="ts">
+
+import { useRoute } from 'vue-router'
+import issues from '@/data/issues.json'
+
+import MagazineSwiper from '@/components/magazine/MagazineSwiper.vue'
+
+const route = useRoute()
+
+const slug = route.params.slug
+
+const issue = issues.find(i => i.slug === slug)
+
+</script>
