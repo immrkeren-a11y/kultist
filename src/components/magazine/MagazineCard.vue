e608a4a61a0ec06f63bdd032df86f6c35da8031f
Сергей Процай <spro1103@mail.ru>
2026-03-05 03:11:46 +0300

    no head

--- src/components/magazine/MagazineCard.vue
+++ src/components/magazine/MagazineCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="card">
+    <h1>{{ magazine.title }}</h1>
     <img :src="magazine.cover" :alt="magazine.title" />
-    <h3>{{ magazine.title }}</h3>
     <p>{{ magazine.year }}</p>
   </div>
 </template>
@@ -16,12 +16,20 @@ defineProps<{
 
 <style>
 .card {
-  border: 1px solid #ddd;
-  padding: 15px;
-  border-radius: 8px;
+  min-width: 368px;
+  aspect-ratio: 1 / 1.414;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
 }
 
+h1{
+  text-transform: uppercase;
+  font-family:'Zhirok', sans-serif;
+}
 .card img {
+  min-width: 368px;
   width: 100%;
   height: auto;
 }
